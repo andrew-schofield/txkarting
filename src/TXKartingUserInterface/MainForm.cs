085e141c05daf52e3c0a3af848f4a2d34bacfed6
unknown <scha@Pagoda.tessella.co.uk>
2013-09-01 16:56:23 +0100

    Add initial skeleton project for TXKarting

---
+++ src/TXKartingUserInterface/MainForm.cs
@@ -0,0 +1,18 @@
+﻿namespace Org.TXCamp.TXKarting.TXKartingUserInterface
+{
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The main form for the TXKarting application
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainForm class.
+        /// </summary>
+        public MainForm()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/TXKartingUserInterface/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Org.TXCamp.TXKarting.TXKartingUserInterface
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The main class for the TXKarting application
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
