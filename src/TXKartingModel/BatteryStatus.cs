28f2b2855b4ef7d45c9741249cc199b126cabf14
Andrew Schofield <andrew@fire-salamander.co.uk>
2013-09-01 18:07:20 +0100

    Added model classes necessary to write a message parser

---
+++ src/TXKartingModel/BatteryStatus.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BatteryStatus.cs" company="txcamp.org">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Org.TXCamp.TXKarting.TXKartingUserInterface
+{
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public enum BatteryStatus
+    {
+        /// <summary>
+        /// The battery is nearly dead
+        /// </summary>
+        EMPTY,
+
+        /// <summary>
+        /// The battery power is running low
+        /// </summary>
+        LOW,
+
+        /// <summary>
+        /// The battery power is normal
+        /// </summary>
+        OK,
+
+        /// <summary>
+        /// The battery is full
+        /// </summary>
+        FULL
+    }
+}
---
+++ src/TXKartingModel/TXID.cs
@@ -0,0 +1,38 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TXID.cs" company="txcamp.org">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Org.TXCamp.TXKarting.TXKartingUserInterface
+{
+    /// <summary>
+    /// A 16 bit ID for identifying karts and drivers
+    /// </summary>
+    public class TXID
+    {
+        /// <summary>
+        /// The internal representation of the TXID.
+        /// In this case, a simple short. (16 bits).
+        /// </summary>
+        private readonly short internalID;
+
+        /// <summary>
+        /// Initializes a new instance of the TXID class.
+        /// </summary>
+        /// <param name="id">A 16 bit identifier</param>
+        public TXID(short id)
+        {
+            this.internalID = id;
+        }
+
+        /// <summary>
+        /// Gets the ID of the current kart
+        /// </summary>
+        /// <returns>A 16 bit identifier</returns>
+        public short GetID()
+        {
+            return this.internalID;
+        }
+    }
+}
